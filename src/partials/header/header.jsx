bddedf2b18edb8c705a712890119db15d715b5b4
Thorsten Kober <tkober@gmail.com>
2024-04-07 13:23:32 -0400

    basic styling

--- src/partials/header/header.jsx
+++ src/partials/header/header.jsx
@@ -1,5 +1,5 @@
 import styles from "../../styles/header.module.css";
 
 export default function Header() {
-    return <header className={styles.header}>Photos Jastified!</header>;
+    return <header className={styles.header}>Photos Justified!</header>;
 }
