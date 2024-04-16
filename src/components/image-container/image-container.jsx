df6e27b990b126baf116f7b6cf28f60bae92edcd
Thorsten Kober <tkober@gmail.com>
2024-04-16 13:34:05 -0400

    use image component

--- src/components/image-container/image-container.jsx
+++ src/components/image-container/image-container.jsx
@@ -1,10 +1,10 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
 import styles from "./image-container.module.css";
 
 export default function ImageContainer({src, alt, width, height}) {
     return (
         <div className={styles["image-container"]}>
-            <img className={styles.image} src={src} alt={alt} width={width} height={height} />
+            <Image className={styles.image} src={src} alt={alt} width={width} height={height} />
         </div>
     )
 }
